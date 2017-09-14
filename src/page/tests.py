0a5bc5b01337d0d309879e17adbdcaa4eb440544
Barnabás Czémán <trabarni@gmail.com>
2017-09-14 16:21:10 +0200

    Clear unused imports

--- src/page/tests.py
+++ src/page/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import Client
+
+
+class SimpleTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        pass
